4333c70f110a1085bca45a3af27d0220d54e2b35
rcowan <ryancowan@gmail.com>
2016-01-20 11:23:09 -0600

    first draft of finding consecutive integer groups adding to a given total, thanks PC

---
+++ coding-problems/integer_stream.rb
@@ -0,0 +1,21 @@
+# Integer Stream - given an array of integers and a target integer, find all
+# the groups of consecutive integers in the array that sum to the target.
+
+stream = [1,3,5,2,3,5,9,2,1,1,4,8,7,2,2,2,2,4,6]
+target = 8
+
+stream.each_with_index do |number, index|
+  group = [number]
+
+  count = index + 1
+  while group.reduce(:+) <= target do
+    stream[count].nil? ? exit : group << stream[count]
+
+    count += 1
+
+    if group.reduce(:+) == target
+      puts "Group: #{group}"
+      puts "...indexes: #{index} - #{count - 1}"
+    end
+  end
+end
