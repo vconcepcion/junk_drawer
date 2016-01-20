060387869393e254f9c0b87c412eba5ed8c18060
Valerie Concepcion <vconcepcion@enova.com>
2016-01-20 17:47:14 -0600

    Integer stream solution

--- coding-problems/integer_stream.rb
+++ coding-problems/integer_stream.rb
@@ -2,20 +2,17 @@
 # the groups of consecutive integers in the array that sum to the target.
 
 stream = [1,3,5,2,3,5,9,2,1,1,4,8,7,2,2,2,2,4,6]
-target = 2
+target = 6
+target_groups = []
 
-stream.each_with_index do |number, index|
-  group = [number]
-
-  count = index + 1
-  while group.reduce(:+) <= target do
-    if group.reduce(:+) == target
-      puts "Group: #{group}"
-      puts "...indexes: #{index} - #{count - 1}"
-    end
-
-    stream[count].nil? ? exit : group << stream[count]
-
-    count += 1
+stream.each_with_index do |number, i|
+  offset = 0
+  loop do
+    group = stream[i-offset..i]
+    sum = group.reduce(&:+)
+    target_groups << group if sum == target
+    offset += 1
+    break if offset > i || sum >= target
   end
 end
+puts "Groups: #{target_groups}"
